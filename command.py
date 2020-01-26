540e86c22761db7e44ccc00f7510c84d5650696a
WeijieXian <jackyalwaysgood@gmail.com>
2020-01-26 16:30:35 +0000

    the git and command code upload to git

---
+++ command.py
@@ -0,0 +1,11 @@
+# ls
+# ls -l
+# clear
+# cd (change direction)
+# mkdir 文件名（make direction)
+# pwd （展示你现在在什么位置）
+# cd .. （回到上一层）
+# rm 文件名 （删除文件）
+# rmdir 文件名 （删除direction)
+# touch 文件名 （开一个文件：py/html/js/ ect..)
+# 更多信息 ：https://blog.csdn.net/weixin_43606158/article/details/103107157
---
+++ git.py
@@ -0,0 +1,26 @@
+# git rm --cached + 文件名 （去除在waitting list 的文件）
+# git clone + URL (把github上别人或者自己的Project下载到自己的电脑上)
+# git status
+# git add + 文件名
+# git add . （上传所有文件）
+# git add *. html (只上传html文件)
+# git commit -m '+留言' （commit在waitting list里的文件）
+# git push origin master (把commit的文件上传到master里)
+# git push origin +branch名 （把commit的文件上传到branch里）
+# git branch (查看git的branch信息)
+# git branch +文件名 （开一个branch 文件）
+# git chechout +文件名 （把location 转换到这个文件的Location）
+# git branch +文件名 -D (删除文件)
+# git init (initilize a folder)
+# git pull (把别人修改最新的respository下载下来)
+# git config --list (查看configuration 里的信息)
+# git config user.name (查看configuration 里的用户名)
+# git config user.username (查看configuration 里用户名的邮箱)
+####
+####
+# 1. Creat new folder
+# 2. Creat py/html/css/js project that you need
+# 3. Use atom 去打代码
+# 4. Initialize folder as a git respository by typing "git init"
+# 5. git config --global user.name '+github用户名'
+# 6. git config --global user.email “+gitbuh邮箱”
